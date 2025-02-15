0ce4b122ad0cebd9147cdfb868222bdd7b441f7c
GraceC-326 <grace.a.cochran@maine.edu>
2025-02-15 15:10:59 -0500

    Updated PatientTester.java
    I called and wrote all of the necessary methods and lines needed. I just need to fix PatientManager.java now.

--- PatientTester.java
+++ PatientTester.java
@@ -1,3 +1,21 @@
 public class PatientTester {
-    
+
+    public static void main(String[] args){
+
+        PatientManager doctor = new PatientManager();
+        System.out.println(doctor);
+
+        doctor.pList.add(new Patient(1, 200));
+        doctor.pList.add(new Patient(2, 400));
+        doctor.pList.add(new Patient(3, 600));
+        doctor.pList.add(new Patient(4, 800));
+        System.out.println(doctor);
+
+        doctor.caffeineAbsorption();
+        doctor.caffeineAbsorption();
+        System.out.println(doctor);
+
+        doctor.removePatient(3);
+        System.out.println(doctor);
+    }
 }
