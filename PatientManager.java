2f3d2e166de4d9f51561dcbccf46e531d46f2b9a
GraceC-326 <grace.a.cochran@maine.edu>
2025-02-15 13:13:12 -0500

    Updated PatientManager.java
    Imported ArrayLists, and made both an ArrayList and default constructor for PatientManager.java

--- PatientManager.java
+++ PatientManager.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
 public class PatientManager {
+    
+    ArrayList<Patient> plist;
+
+    public PatientManager(){
+        plist = new ArrayList<Patient>(10);
+    }
+
     
 }
