0bcdd227983ec62f816af9c375e93d4bcf83c318
GraceC-326 <grace.a.cochran@maine.edu>
2025-02-15 13:31:18 -0500

    Updated PatientManager.java
    Added methods admitPatient() and removePatient().

--- PatientManager.java
+++ PatientManager.java
@@ -1,11 +1,26 @@
 import java.util.ArrayList;
 public class PatientManager {
     
-    ArrayList<Patient> plist;
+    ArrayList<Patient> pList;
 
     public PatientManager(){
-        plist = new ArrayList<Patient>(10);
+        pList = new ArrayList<Patient>(10);
     }
 
-    
+    public int admitPatient(Patient beingAdmitted){
+        for (int i=0; i < pList.size(); i++){
+            if (pList.get(i) == null){
+                pList.set(i, beingAdmitted);
+                int index = i;
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public Patient removePatient(int indexNum){
+        Patient pRemoved = pList.get(indexNum);
+        pList.set(indexNum, null);
+        return pRemoved;
+    }
 }
